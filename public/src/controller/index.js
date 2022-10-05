880cbe027853f6f02bec0266d26fe75c41b31c8f
Kodeneko <ladysun.freedom@gmail.com>
2022-10-05 19:48:24 +0200

    [dev] untrack src/public + previous env logic

--- public/src/controller/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getChangeLang = exports.getDoc = exports.getSubject = exports.getMain = void 0;
-var page_1 = require("./page");
-Object.defineProperty(exports, "getMain", { enumerable: true, get: function () { return page_1.getMain; } });
-Object.defineProperty(exports, "getSubject", { enumerable: true, get: function () { return page_1.getSubject; } });
-Object.defineProperty(exports, "getDoc", { enumerable: true, get: function () { return page_1.getDoc; } });
-Object.defineProperty(exports, "getChangeLang", { enumerable: true, get: function () { return page_1.getChangeLang; } });
--- public/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pageRouter = void 0;
-var page_1 = require("./page");
-Object.defineProperty(exports, "pageRouter", { enumerable: true, get: function () { return __importDefault(page_1).default; } });
